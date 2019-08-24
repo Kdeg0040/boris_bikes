14b17a713fdfbe02d1d9059cee7dec5b6ce21180
karlo <deguzmankarlo@gmail.com>
2019-08-24 22:23:13 +0100

    Chapter 17 complete - set initialization default

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
-
-  attr_reader :bikes
   DEFAULT_CAPACITY = 20
 
-  def initialize
+  attr_reader :bikes
+  attr_accessor :capacity
+
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
 
   def release_bike
@@ -22,7 +24,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.size >= DEFAULT_CAPACITY
+    @bikes.size >= @capacity
   end
 
   def empty?
